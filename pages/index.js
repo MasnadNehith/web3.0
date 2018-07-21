6f3cc79369aabd3ba10ff0f509b1c9588abfebd2
masnad <masnad.nehith@gmail.com>
2018-07-21 20:27:53 +0300

    Setting Up Pages With Next

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React ,{Component} from 'react';
+
+class Lottery extends Component{
+  render(){
+    return (
+       <h1> Decentralized Lottery Application </h1>
+    )
+  }
+}
+
+export default Lottery;
